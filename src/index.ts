33bbefcb581280f60ef27aba805c94c35a7f7ac4
Beam <supawit04591@pcccr.ac.th>
2023-07-30 19:18:57 +0700

    add express

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import bodyParser from 'body-parser'
+import router from './server.js'
+
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+app.use('/api', router)
+
+app.listen(PORT, () => {
+    console.log(`[server]: Server is running at http://localhost:${PORT}`)
+})
