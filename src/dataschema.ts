51a2a7c8c6bf3256b80c323581b24df7cc719d2d
Beam <supawit04591@pcccr.ac.th>
2023-07-31 10:49:56 +0700

    add prisma and GET/POST Api endpoints for IoT device

---
+++ src/dataschema.ts
@@ -0,0 +1,47 @@
+import { NextFunction, Request, Response } from 'express'
+import { z, AnyZodObject } from 'zod'
+
+const dataSchema = z.object({
+    body: z.object({
+        machineId: z.string({
+            required_error: 'machineId is required',
+        }),
+        cilentTime: z.string({
+            required_error: 'cilentTime is required',
+        }),
+        voltage: z.number({
+            required_error: 'volatage is required',
+        }),
+        current: z.number({
+            required_error: 'current is required',
+        }),
+        power: z.number({
+            required_error: 'power is required',
+        }),
+    }),
+})
+
+const dataGetSchema = z.object({
+    query: z.object({
+        machineId: z.string({
+            required_error: 'machineId is required',
+        }),
+    }),
+})
+
+const validate =
+    (schema: AnyZodObject) =>
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            await schema.safeParseAsync({
+                body: req.body,
+                query: req.query,
+                params: req.params,
+            })
+            return next()
+        } catch (error) {
+            return res.status(400).json(error)
+        }
+    }
+
+export { dataSchema, validate, dataGetSchema }
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,65 @@
-import { Router } from 'express'
+import { Router, Request, Response } from 'express'
+
+import { PrismaClient } from '@prisma/client'
+import { dataGetSchema, dataSchema, validate } from './dataschema.js'
+const prisma = new PrismaClient()
 
 const router = Router()
 
-router.get('/', (req, res) => {
-    res.send('Hello World')
+// GET /api/electric
+router.get('/electric', async (req: Request, res: Response) => {
+    const data = await prisma.electric.findMany({
+        orderBy: {
+            serverTime: 'asc',
+        },
+        take: 10,
+    })
+
+    return res.json(data).status(200)
 })
 
-router.get('/test', (req, res) => {
-    res.send('Hello World Test')
-})
+// GET /api/electric?MachineId=xxx?limit=10
+router.get(
+    '/electric',
+    validate(dataGetSchema),
+    async (req: Request, res: Response) => {
+        const { machineId } = req.query
+        const data = await prisma.electric.findMany({
+            where: {
+                machineId: machineId as string,
+            },
+            orderBy: {
+                serverTime: 'asc',
+            },
+            take: 10,
+        })
+
+        return res.json(data).status(200)
+    }
+)
+
+// POST /api/electric
+router.post(
+    '/electric',
+    validate(dataSchema),
+    async (req: Request, res: Response) => {
+        let serverDate: Date = new Date()
+        const { machineId, cilentTime, voltage, current, power } = req.body
+
+        const data = await prisma.electric.create({
+            data: {
+                machineId: machineId,
+                clientTime: cilentTime,
+                serverTime: serverDate,
+                voltage: voltage,
+                current: current,
+                power: power,
+            },
+        })
+
+        console.log(serverDate, req.body, data)
+        return res.json({ ...req.body })
+    }
+)
 
 export default router
